624caaa728fc69b8c894c20e22b269fd227a99ff
VuongTuanAnh165 <92902120+VuongTuanAnh165@users.noreply.github.com>
2021-10-29 14:34:44 +0700

    updateviewimg

--- pages/post/index.php
+++ pages/post/index.php
@@ -20,15 +20,38 @@ $sql_post_new = "SELECT * FROM post WHERE post_active=1 ORDER BY post_datetime_u
 $post_new = $db->fetchdata($sql_post_new);
 ?>
 
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $post_title = $_POST['post_title'];
+    if ($post_title != '') {
+        $sql_post = "SELECT * FROM post WHERE post_title LIKE '%$post_title%' AND post_active=1";
+        $post = $db->fetchdata($sql_post);
+    } else {
+        $sql_post = "SELECT * FROM post WHERE post_type_id=$id";
+        $post = $db->fetchdata($sql_post);
+    }
+}
+?>
+
 <!-- page title -->
 <div class="page-title">
     <div class="container-fluid">
         <div class="row">
-            <div style="background-image: url(./photo/<?php echo $post[0]['post_image1']?>);" class="inner-title">
+            <div style="background-image: url(./photo/<?php echo $post[0]['post_image1'] ?>);" class="inner-title">
                 <div class="overlay-image"></div>
                 <div class="banner-title">
                     <div class="page-title-heading">
-                        Tin tức<br><?php echo $post_type_id['post_type_title']?>
+                        Tin tức<br>
+                        <?php 
+                            if(isset($post_title)) 
+                            {
+                                echo " Liên quan đến '".$post_title."'";
+                            }
+                            else
+                            {
+                                echo $post_type_id['post_type_title'];
+                            }    
+                        ?>
                     </div>
                     <div class="page-title-content link-style6">
                         <span><a class="home" href="<?php echo base_url() ?>index.php">Trang chủ</a></span><span><a class="page-title-content-inner" href="<?php echo base_url() ?>pages/post_type/index.php">Nhóm tin tức</a></span><span class="page-title-content-inner">Tin tức</span>
@@ -51,26 +74,26 @@ $post_new = $db->fetchdata($sql_post_new);
             </div>
             <div class="col-md-8">
                 <div class="post-wrap">
-                    <?php foreach($post as $item): ?>
+                    <?php foreach ($post as $item) : ?>
                         <article class="article-1">
                             <div class="image-box">
                                 <div class="image">
                                     <img src="<?php echo base_img('post') ?>photo/<?php echo $item['post_image1'] ?>" alt="image">
                                 </div>
                                 <div class="date-image">
-                                    <p><?php echo $item['post_datetime_update']?></p>
+                                    <p><?php echo $item['post_datetime_update'] ?></p>
                                 </div>
                             </div>
                             <div class="content-box">
                                 <div class="content-art">
                                     <a href="blog-detail.html" class="section-heading-jost-size28">
-                                        <?php echo $item['post_title']?>
+                                        <?php echo $item['post_title'] ?>
                                     </a>
                                     <p class="desc-content-box text-decs">
                                         <?php echo $item['post_description'] ?>
                                     </p>
                                     <div class="link-style2">
-                                        <a href="post_details.php?id=<?php echo $item['post_id']?>" class="read-more">
+                                        <a href="post_details.php?id=<?php echo $item['post_id'] ?>" class="read-more">
                                             Xem thêm<i class="fas fa-long-arrow-alt-right"></i>
                                         </a>
                                     </div>
@@ -91,22 +114,22 @@ $post_new = $db->fetchdata($sql_post_new);
                             Nhóm tin tức
                         </h3>
                         <ul class="list-category">
-                            <?php foreach($post_type as $item): ?>
-                                <li><a href="<?php echo base_url() ?>pages/post/index.php?id=<?php echo $item['post_type_id'] ?>"><?php echo $item['post_type_title']?></a></li>
+                            <?php foreach ($post_type as $item) : ?>
+                                <li><a href="<?php echo base_url() ?>pages/post/index.php?id=<?php echo $item['post_type_id'] ?>"><?php echo $item['post_type_title'] ?></a></li>
                             <?php endforeach ?>
                         </ul>
                     </div>
                     <div class="widget widget_lastest">
                         <h2 class="widgets-side-bar-title"><span>Tin tức mới</span></h2>
                         <ul class="lastest-posts data-effect clearfix">
-                            <?php foreach($post_new as $item): ?>
+                            <?php foreach ($post_new as $item) : ?>
                                 <li class="clearfix">
                                     <div class="thumb data-effect-item has-effect-icon">
-                                        <img src="<?php echo base_img('post')?>photo/<?php echo $item['post_image1']?>" alt="Image">
+                                        <img src="<?php echo base_img('post') ?>photo/<?php echo $item['post_image1'] ?>" alt="Image">
                                     </div>
                                     <div class="text">
-                                        <h3><a href="post_details.php?id=<?php echo $item['post_id']?>" class="title-thumb"><?php echo $item['post_title']?></a></h3>
-                                        <a href="#" class="date"><?php echo $item['post_datetime_update']?></a>
+                                        <h3><a href="post_details.php?id=<?php echo $item['post_id'] ?>" class="title-thumb"><?php echo $item['post_title'] ?></a></h3>
+                                        <a href="#" class="date"><?php echo $item['post_datetime_update'] ?></a>
                                     </div>
                                 </li>
                             <?php endforeach ?>
